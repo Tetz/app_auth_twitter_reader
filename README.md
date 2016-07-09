0c114ec69b8e556bc18e15729cf9dbab2ec66b83
Toshiya Komoda <toshiya.komoda@gmail.com>
2016-07-10 02:21:37 +0900

    add app_aut_twitter_reader.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # AppAuthTwitterReader
 
-Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/app_auth_twitter_reader`. To experiment with that code, run `bin/console` for an interactive prompt.
+Download user timelines from twitter api. The output format is csv. User timelines includes
 
-TODO: Delete this and the text above, and describe your gem
+tweets by the user.
+retweets by the user.
+Currently, the maximum number of tweets you can get is limited to less than 3200 due to the API limitation. (See https://dev.twitter.com/rest/reference/get/statuses/user_timeline)
 
 ## Installation
 
@@ -22,7 +24,30 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+# Requirement
+
+ * Ruby > 2.1.0
+
+# Get twitter api key and secret
+
+The script use application only authentication.
+Register as a twitter developer and get api key and secret.
+
+[Twitter Application Management Console](https://apps.twitter.com/)
+
+# Usage
+
+Assume that your key and secret is written in `./api_key` and `./api_secret`.
+
+```bash
+
+# get access token (only once)
+./get_timeline  --key=`cat ./api_key` --secret=`cat ./api_secret` > ./access_token
+
+# get user timeline using the access token.
+./get_timeline  --token=`cat ./access_token` --screen_name twitter
+
+```
 
 ## Development
 
--- lib/app_auth_twitter_reader.rb
+++ lib/app_auth_twitter_reader.rb
@@ -1,5 +1,54 @@
 require "app_auth_twitter_reader/version"
+require "net/https"
+require "uri"
+require "base64"
+require "json"
+require "optparse"
 
 module AppAuthTwitterReader
-  # Your code goes here...
+
+  # twitter API client via application-only authentication.
+  # See:https://dev.twitter.com/oauth/application-only
+  class Client
+
+    def initialize
+      @token_endpoint    = 'https://api.twitter.com/oauth2/token'
+      @timeline_endpoint = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+    end
+
+    def auth_for_token_endpoint(key, secret)
+      return 'Basic ' + Base64.strict_encode64(key + ':' + secret)
+    end
+
+    def get_access_token(key:, secret:)
+      uri  = URI.parse(@token_endpoint)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      request = Net::HTTP::Post.new(uri.request_uri)
+      request['Authorization'] = self.auth_for_token_endpoint(key, secret)
+      request.set_form_data({"grant_type" => "client_credentials"})
+
+      response = http.request(request)
+      data = JSON.parse(response.body)
+      return data['access_token'] || "may be key or secret is invalid"
+    end
+
+    # See: https://dev.twitter.com/rest/reference/get/statuses/user_timeline
+    def get_user_timeline(access_token:, screen_name:, count:, max_id:nil)
+      query_string = "?" + "count=#{count}&screen_name=#{screen_name}"
+      query_string += "&max_id=#{max_id}" unless max_id.nil?
+      uri = URI.parse( @timeline_endpoint + query_string)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      request  = Net::HTTP::Get.new(uri)
+      request['Authorization'] = 'Bearer ' + access_token
+      response = http.request(request)
+      if response.is_a?(Net::HTTPSuccess) then
+        return [JSON.parse(response.body), nil]
+      else
+        return [nil, "twitter api returned error. #{response.body}"]
+      end
+    end
+
+  end
 end
